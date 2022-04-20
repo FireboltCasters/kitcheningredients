2ceabe107992ae199a0f81b57b912042e2253032
Nils <nilsbaumgartner1994@gmail.com>
2022-04-20 16:21:52 +0200

    Fix?

---
+++ lib/typescript/src/ignoreCoverage/TextWithIcon.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export declare const TextWithIcon: (props: any) => JSX.Element;
--- src/tests/ExampleTest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Example_1 = __importDefault(require("../Example"));
-test('Example Test', async () => {
-    expect(Example_1.default.multiply(3, 4)).toBe(12);
-});
