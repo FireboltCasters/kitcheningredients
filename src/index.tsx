9f7e7c3327cc8acfa5eb86320e676e77c7986614
Nils <nilsbaumgartner1994@gmail.com>
2022-04-20 21:41:38 +0200

    Test With Components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+export * from "./ignoreCoverage/TextWithIcon";
+
+import {OtherTextWithIcon} from "./ignoreCoverage/OtherTextWithIcon";
+export {OtherTextWithIcon};
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
