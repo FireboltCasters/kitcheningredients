f86393afc21a209c5449af8190fdbbabd71e997e
Nils <nilsbaumgartner1994@gmail.com>
2022-04-22 01:11:51 +0200

    Fix missing import

--- src/ignoreCoverage/KitchenHelper/App.tsx
+++ src/ignoreCoverage/KitchenHelper/App.tsx
@@ -24,7 +24,9 @@ export default class App extends React.Component<any, any>{
 		super(props);
 
 		ConfigHolder.instance = this;
-    ConfigHolder.storage = new MyDirectusStorage();
+		if(!!ConfigHolder.storage){
+      ConfigHolder.storage = new MyDirectusStorage();
+    }
 
     RouteRegisterer.register();
     RouteRegisterer.loadDrawerScreens();
