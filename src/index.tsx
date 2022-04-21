355da930b052712bb318496dff13d8135e2c9dd2
Nils <nilsbaumgartner1994@gmail.com>
2022-04-22 00:34:40 +0200

    Fix tsc bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
-import {TextWithIcon} from "./ignoreCoverage/components/TextWithIcon";
+import {TextWithIcon} from "./ignoreCoverage/KitchenHelper/components/TextWithIcon";
 export {TextWithIcon};
-
 
 import {ConfigHolder} from "./ignoreCoverage/KitchenHelper/ConfigHolder";
 export {ConfigHolder}
 
-import {ConfigHolderTest} from "./ignoreCoverage/components/ConfigHolderTest";
-export {ConfigHolderTest}
+import App from "./ignoreCoverage/KitchenHelper/App";
+export {App}
+
+import {Root} from "./ignoreCoverage/KitchenHelper/navigation/RootComponent";
+export {Root}
