5c179649e58b300ecd26684b09dd331d22416757
Nils <nilsbaumgartner1994@gmail.com>
2022-04-21 15:19:33 +0200

    Full Helper Export fix

--- src/ignoreCoverage/storage/WebStorageWrapper.ts
+++ src/ignoreCoverage/storage/WebStorageWrapper.ts
@@ -4,7 +4,7 @@ import {StorageImplementationInterface} from "./StorageImplementationInterface";
 export class WebStorageWrapper implements StorageImplementationInterface/** extends Storage */{
     private webstorage: any;
 
-    constructor(webstorage: Storage) {
+    constructor(webstorage: any) {
         this.webstorage = webstorage;
     }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,3 @@ export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
-import {TextWithIcon} from "./ignoreCoverage/components/TextWithIcon";
-export {TextWithIcon};
-
-import {OtherTextWithIcon} from "./ignoreCoverage/OtherTextWithIcon";
-export {OtherTextWithIcon};
