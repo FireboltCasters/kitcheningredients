08c6849540ace33ca2ef9e112bd04490b7f09612
Nils <nilsbaumgartner1994@gmail.com>
2022-04-21 13:27:32 +0200

    Added Buttons

---
+++ src/ignoreCoverage/buttons/TransparentButton.tsx
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import React, {FunctionComponent} from 'react';
+import {Button, Text} from "native-base";
+import {IButtonProps} from "native-base/src/components/primitives/Button/types";
+import {IBoxProps} from "native-base/src/components/primitives/Box/index";
+
+export const TransparentButton: FunctionComponent<IButtonProps & IBoxProps> = (props) => {
+
+	let content = props.children;
+
+	return (
+		<Button minWidth={154} bgColor={"#00000000"} {...props}>
+			{content}
+		</Button>
+	)
+}
