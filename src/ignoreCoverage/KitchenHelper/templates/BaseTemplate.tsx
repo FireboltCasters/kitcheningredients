6ae6c54d6925293a40915a6d6e7dfbab65f3e95b
Nils <nilsbaumgartner1994@gmail.com>
2022-05-01 20:35:13 +0200

    Readded Scrollview

--- src/ignoreCoverage/KitchenHelper/templates/BaseTemplate.tsx
+++ src/ignoreCoverage/KitchenHelper/templates/BaseTemplate.tsx
@@ -6,6 +6,7 @@ import {Layout} from "./Layout";
 import {CloneChildrenWithProps} from "../helper/CloneChildrenWithProps";
 import {ShowMoreGradientPlaceholder} from "../utils/ShowMoreGradientPlaceholder";
 import {EmptyTemplate} from "./EmptyTemplate";
+import {ScrollViewWithGradient} from "../utils/ScrollViewWithGradient";
 
 export const BaseTemplate = ({
 								 children,
@@ -21,12 +22,14 @@ export const BaseTemplate = ({
 
 	return(
 		<EmptyTemplate {...props}>
+      <ScrollViewWithGradient hideGradient={true} style={{width: "100%", height: "100%"}} >
 				<BreakPointLayout >
 					<Box style={{height: "100%", alignItems: "flex-start", width: "100%"}}>
 						{childrenWithProps}
             <ShowMoreGradientPlaceholder />
 					</Box>
 				</BreakPointLayout>
+      </ScrollViewWithGradient>
 		</EmptyTemplate>
 	)
 }
