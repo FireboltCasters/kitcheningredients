147af6bf910a491b9d90e576e9b979779f781211
Nils <nilsbaumgartner1994@gmail.com>
2022-05-18 23:36:08 +0200

    Change PluginInterface to PluginClass

--- src/ignoreCoverage/PluginInterface.ts
+++ src/ignoreCoverage/PluginInterface.ts
@@ -1,14 +1,38 @@
-export interface PluginInterface{
-    initApp();
-    registerRoutes();
-    onLogout(error);
-    onLogin(user, role);
-    getSynchedStateKeysClass();
-    getStorageKeysClass();
-    getAboutUsComponent();
-    getPrivacyPolicyComponent();
-    getTermsAndConditionsComponent();
-    getHomeComponent();
-    getSettingsComponent();
-    getCustomProjectLogoComponent();
+export abstract class PluginInterface{
+    initApp(){
+
+    }
+    registerRoutes(){
+
+    }
+    onLogout(error){
+
+    }
+    onLogin(user, role){
+
+    }
+    getSynchedStateKeysClass(){
+      return null;
+    }
+    getStorageKeysClass(){
+      return null;
+    }
+    getAboutUsComponent(){
+      return null;
+    }
+    getPrivacyPolicyComponent(){
+      return null;
+    }
+    getTermsAndConditionsComponent(){
+      return null;
+    }
+    getHomeComponent(){
+      return null;
+    }
+    getSettingsComponent(){
+      return null;
+    }
+    getCustomProjectLogoComponent(){
+      return null;
+    }
 }
