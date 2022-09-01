64ff74f4642356f54ac67f084119d72ad462842a
Nils <nilsbaumgartner1994@gmail.com>
2022-09-01 14:30:56 +0200

    Fix

--- app/src/api/src/ignoreCoverage/KitchenHelper/configureWebpackConfig.js
+++ app/src/api/src/ignoreCoverage/KitchenHelper/configureWebpackConfig.js
@@ -1,11 +1,11 @@
-module.exports = async function (config) {
+module.exports = async function (config, customProcEnv) {
   // Customize the config before returning it.
   if(!config.optimization){
     config.optimization = {};
   }
   // Disable minification, because we have named routing
   config.optimization.minimize = false;
-  let procEnv = process.env;
+  let procEnv = customProcEnv || process.env;
   let BASE_PATH = procEnv.BASE_PATH;
   if(!!BASE_PATH){
     // Add publicPath to given BASE_PATH from docker-compose.yml
