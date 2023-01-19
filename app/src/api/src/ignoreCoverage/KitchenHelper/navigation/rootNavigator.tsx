26d0a3e5dc6cb3d80d4cf1d6320f2a9483e1761b
Nils <nilsbaumgartner1994@gmail.com>
2023-01-19 08:43:21 +0100

    Fix for native

--- app/src/api/src/ignoreCoverage/KitchenHelper/navigation/rootNavigator.tsx
+++ app/src/api/src/ignoreCoverage/KitchenHelper/navigation/rootNavigator.tsx
@@ -1,36 +1,39 @@
 // @ts-nocheck
 import React, {useEffect} from 'react';
-import {RegisteredRoutesMap} from "./RegisteredRoutesMap";
-import {useBreakpointValue, useTheme, View, Text} from "native-base";
-import {CustomDrawerContent} from "./CustomDrawerContent";
-import {ConfigHolder} from "../ConfigHolder";
+import {Text, View} from "native-base";
 import {RouteRegisterer} from "./RouteRegisterer";
-import {Layout} from "../templates/Layout";
-import {RequiredSynchedStates} from "../synchedstate/RequiredSynchedStates";
-import {useSynchedState} from "../synchedstate/SynchedState";
-import {useSynchedJSONState} from "../synchedstate/SynchedState";
 import {NavigatorHelper} from "../navigation/NavigatorHelper";
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {TouchableOpacity} from "react-native";
+import {PlatformHelper} from "kitcheningredients";
 
 export const RootStack = (props) => {
-  const initialRouteName = window.location.hash.substr(1);
+
+  let initialRouteName = "Check";
+
+  if(PlatformHelper.isWeb()){
+    initialRouteName = window.location.hash.substr(1);
+  }
 
   let Drawer = RouteRegisterer.getDrawer();
 
   const navigateAndSetHash = (navigation, routeName) => {
     navigation.navigate(routeName);
-    window.location.hash = routeName;
+    if(PlatformHelper.isWeb()){
+      window.location.hash = routeName;
+    }
   };
 
   useEffect(() => {
-    function handleHashChange() {
+    if(PlatformHelper.isWeb()){
+      function handleHashChange() {
         const currentRoute = window.location.hash.substr(1);
         //navigation.navigate(currentRoute);
         NavigatorHelper.navigateToRouteName(currentRoute)
+      }
+      window.addEventListener('hashchange', handleHashChange);
+      return () => window.removeEventListener('hashchange', handleHashChange);
     }
-    window.addEventListener('hashchange', handleHashChange);
-    return () => window.removeEventListener('hashchange', handleHashChange);
   }, []);
 
   return (
