7d6c9efbcba8d815d84917eb7611ad8be486b98b
Nils <nilsbaumgartner1994@gmail.com>
2023-01-30 02:36:42 +0100

    Fix directusIcon

--- app/src/api/src/ignoreCoverage/KitchenHelper/project/DirectusIcon.tsx
+++ app/src/api/src/ignoreCoverage/KitchenHelper/project/DirectusIcon.tsx
@@ -4,11 +4,11 @@ import {Icon} from "../components/Icon";
 import {MaterialIcons} from "@expo/vector-icons";
 import {InterfaceIconProps} from "native-base/lib/typescript/components/primitives/Icon/types";
 
-export const DirectusIcon: FunctionComponent<InterfaceIconProps> = (props) => {
+export const DirectusIcon: FunctionComponent<InterfaceIconProps> = ({name ,...rest}) => {
   // replace all _ with - for icon names
-  let icon = props?.name || "";
+  let icon = name || "";
   icon = icon.replace(/_/g, "-");
 
   let family = MaterialIcons;
-  return <Icon as={family} name={icon} {...props} />
+  return <Icon as={family} name={icon} {...rest} />
 }
